f2e547419710b1ce6c6082c9c3d28188e562ffa8
Nasser ALRuwais <nasserbnabdullah@hotmail.com>
2019-12-01 15:21:45 +0300

    Digit-Recognition

---
+++ digitRecognition.py
@@ -0,0 +1,13 @@
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import math
+from mnist import MNIST
+
+data = MNIST(data_dir="data/MNIST/")
+
+print("Size of:")
+print("- Training-set:\t\t{}".format(data.num_train))
+print("- Validation-set:\t{}".format(data.num_val))
+print("- Test-set:\t\t{}".format(data.num_test))
